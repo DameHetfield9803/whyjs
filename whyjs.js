c39fb21e9f7c8ed970814174d1d08c6b38e89aa7
kevinlem0n <126497052+kevin-lem0n@users.noreply.github.com>
2023-06-28 20:10:34 +0700

    Initial commit

---
+++ whyjs.js
@@ -0,0 +1,18 @@
+/*
+whyjs - a better version of JavaScript
+Sometimes I ask myself WHY JavaScript ><
+I mean there's Typescript, but why not make a better JavaScript without Typescript?
+*/
+(function(window) {
+
+    // Selector
+    window.$$ = function(a) {
+        return document.querySelector(a);
+    };
+
+    // Console.log(a)
+    window.print = function (a){
+        console.log(a);
+    }
+
+})(window);
