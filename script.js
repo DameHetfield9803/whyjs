5c75ddb868f1dca9d1e234b7a3de4afe9167f07f
kevinlem0n <126497052+kevin-lem0n@users.noreply.github.com>
2023-06-28 22:49:57 +0700

    there you go

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
-let a = 10;
-let b = 5;
-
 /*
-const result = 
-$do([
-  { when: a > b, 
-    then: "a is bigger" 
-  },
-  { 
-    when: a < b, 
-    then: "b is bigger" 
-  }
-]);
-
-console.log(result);
+DEMO
+- This file provides example whyjs functions
 */
+
+// outputting to console
+$$print("hello world");
+
+// if else
+const a = 10;
+const b = 5;
+const result = $$do([
+  {when: a > b, then: "a is bigger" },
+  {when: a < b, then: "b is bigger" }
+]);
+$$print(result); // output => a is bigger
+
+// alert
+//$$a("hellos");
--- whyjs.js
+++ whyjs.js
@@ -2,19 +2,38 @@
 whyjs - a better version of JavaScript
 Sometimes I ask myself WHY JavaScript ><.
 I mean there's Typescript, but why not make a better JavaScript without Typescript?
+
+IMPORTANT:
+whyjs.js have to be LOADED first before your actual <script> tag.
+
+NOTE:
+Any undefined or null value will be replaced with a 🤡
 */
 (function(window) {
+    
+    // Constants
+    const Z = 0;
 
     // Selector
     // Using $$ to prevent clashing with jQuery :(
     window.$$ = function(a) {
-        return document.querySelector(a);
+        if(a != null){
+            return document.querySelector(a);
+        }
+        if(a == null){
+            return "🤡";
+        }
     };
+
+    // alert
+    window.$$a = function(a) {
+        alert(a);
+    }
 
     /*
         console.log(a)
     */
-    window.print = function (a){
+    window.$$print = function(a){
         if(a == undefined)
             console.log("🤡");
         else
@@ -22,15 +41,12 @@ I mean there's Typescript, but why not make a better JavaScript without Typescri
     }
 
     // if else
-    window.$do = function condition(obj) {
-        for (let i = 0; i < obj.length; i++) {
-          const { when, then } = obj[i];
-          if (when) {
-            return then;
-          }
+    window.$$do = function(o) {
+        for (let i = Z; i < o.length; i++) {
+          const { when, then } = o[i];
+          if (when) {return then;}
         }
     }
-    
 
 
 })(window);
