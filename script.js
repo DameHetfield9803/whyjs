5311f3de8deb3ed75f87a2a20da53e17c6f18ff3
kevinlem0n <126497052+kevin-lem0n@users.noreply.github.com>
2023-07-01 01:41:25 +0800

    Updated syntax
    Syntax now starts with _ (underscore) instead of $$ (double dollar sign)

--- script.js
+++ script.js
@@ -1,19 +1,28 @@
 /*
-DEMO
-- This file provides example whyjs functions
+  This file provides example on how to use whyjs functions
 */
 
 // outputting to console
-$$print("hello world");
+_print("hello world");
 
 // if else
 const a = 10;
 const b = 5;
-const result = $$do([
+const result = _do([
   { when: a > b, then: "a is bigger" },
   { when: a < b, then: "b is bigger" }
 ]);
-$$print(result); // output => a is bigger
+_print(result); // output => a is bigger
 
 // alert
-//$$a("hellos");
+//_a("hellos");
+
+//Get the <h1>Hello</h1> tag and change Hello to OK
+// _("h1")._html("OK")
+
+// Bind value
+/*
+Now when we inspect element, it will look like 
+<h1 bind-value="Lemon">Hello</h1>
+*/ 
+_find("h1")._bind("Lemon");
--- whyjs.js
+++ whyjs.js
@@ -8,7 +8,10 @@ IMPORTANT:
 whyjs.js have to be LOADED first before your actual <script> tag.
 
 NOTE:
-Any undefined or null value will be replaced with a 🤡
+Any undefined or null value will be replaced with a 🤡 
+
+Syntax:
+why js functions starts with _ (underscore)
 */
 (function(window) {
     
@@ -16,8 +19,7 @@ Any undefined or null value will be replaced with a 🤡
     const Z = 0;
 
     // Selector
-    // Using $$ to prevent clashing with jQuery :(
-    window.$$ = function(a) {
+    window._find = function(a) {
         if(a != null){
             return document.querySelector(a);
         }
@@ -26,13 +28,13 @@ Any undefined or null value will be replaced with a 🤡
         }
     };
 
-    // alert
-    window.$$a = function(a) {alert(a);}
+    // Alert
+    window._a = function(a) {alert(a);}
 
     /*
         console.log(a)
     */
-    window.$$print = function(a){
+    window._print = function(a){
         if(a == undefined)
             console.log("🤡");
         else
@@ -49,7 +51,7 @@ Any undefined or null value will be replaced with a 🤡
             { when: a < b, then: "b is bigger" }
         ]);
     */
-    window.$$do = function(o) {
+    window._do = function(o) {
         for (let i = Z; i < o.length; i++) {
           const { when, then } = o[i];
           if (when) {return then;}
@@ -60,17 +62,17 @@ Any undefined or null value will be replaced with a 🤡
     This is for TESTING only
     PLEASE do not use !
     */
-    window.$$js = function(a){
-        if(a.trim() != ""){
-            eval(a);
-        }
-    }
+    // window.$$js = function(a){
+    //     if(a.trim() != ""){
+    //         eval(a);
+    //     }
+    // }
 
     /*
     Sanitize string & remove all HTML special characters
     <h1>Hello</h1> --> h1hello/h1
     */ 
-    window.$$clean = function(a){
+    window._clean = function(a){
         if(a.includes("<")){
             a = a.replaceAll("<", "");
         }
@@ -80,5 +82,34 @@ Any undefined or null value will be replaced with a 🤡
         return a;
     }
 
+    // HTTP methods
+    // window.$$http = function(a){
+    //     var method = a.method;
+    //     var url = a.url;
+    
+    //     if(method == "GET"){
+    //         return fetch(url)
+    //             .then(response => response.json())
+    //             .then(data => {
+    //                 return JSON.stringify(data);
+    //             })
+    //             .catch(error => console.error(error));
+    //     }
+    // }
+    
+    // Inner HTML
+    // This functions accept HTML passed as string
+    window.Object.prototype._html = function(a){
+        this.innerHTML = a;
+    }
+
+    // Bind value to element
+    window.Object.prototype._bind = function(a) {
+        this.value = a;
+        this.setAttribute('bind-value', a);
+    }
+
+    // Custom style
+    
 
 })(window);
